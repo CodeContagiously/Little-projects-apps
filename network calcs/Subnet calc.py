6617846df8f49ab8bc689720a49ec30772f44542
PerryBoh <42015988+CodeContagiously@users.noreply.github.com>
2020-05-13 15:34:01 -0400

    Add files via upload

---
+++ network calcs/Subnet calc.py
@@ -0,0 +1,16 @@
+""" This program performes subnetting computations """
+
+class Net:
+    def __init__():
+
+
+    def IPSubnet(self, IPAddress/CDR):
+        '''return the subnet given IP address and CDR number'''
+
+    def NetRange(self, IPAddress/CDR):
+        ''' returns the IP address range '''
+
+    def subNet(self, IPAddress/CDR, numberOfNet):
+        ''' Using VLSM, segment the network as specified
+            Return each subnet and it's Range'''
+
